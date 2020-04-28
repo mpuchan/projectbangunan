ba354a10cfe5d91bff8e34f7aaa52e775692059a
mpuchan <52643218+mpuchan@users.noreply.github.com>
2020-04-28 21:05:19 +0800

    Register template fix

--- app/src/main/java/com/example/bangunankita/Login.java
+++ app/src/main/java/com/example/bangunankita/Login.java
@@ -2,15 +2,33 @@ package com.example.bangunankita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+        Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
 
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent home=new Intent(Login.this, Register.class);
+                startActivity(home);
+            }
+        });
+
+
+
+    }
+
+    private void init() {
+        signup=findViewById(R.id.signUp);
     }
 }
